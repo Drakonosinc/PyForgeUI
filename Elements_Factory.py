ef3f6bd433a11318f6d77e392b4559f05a1aba22
matias <mat20can@gmail.com>
2025-05-19 17:10:17 -0300

    version 1

--- Elements_Factory.py
+++ Elements_Factory.py
@@ -11,4 +11,6 @@ class ElementsFactory:
         return Text({"screen": self.screen,"font": self.font,"color": self.color,"hover_color": self.hover_color,**config})
     def create_TextButton(self,config:dict):
         return TextButton({"screen": self.screen,"font": self.font,"color": self.color,"hover_color": self.hover_color,"sound_hover": self.sound_hover,"sound_touch": self.sound_touch,**config})
+    def create_PolygonButton(self,config:dict):
+        return PolygonButton({"screen": self.screen,"color": self.color,"hover_color": self.hover_color,"sound_hover": self.sound_hover,"sound_touch": self.sound_touch,**config})
     
