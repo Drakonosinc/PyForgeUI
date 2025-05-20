368ad787f3fecccbbf5d22aa663067fff33a30c5
matias <mat20can@gmail.com>
2025-05-20 15:23:08 -0300

    version 1

--- Elements/Elements_interface.py
+++ Elements/Elements_interface.py
@@ -1,9 +1,5 @@
 import pygame
 from pygame.locals import *
-    def pressed_button(self,rect,pressed_mouse,mouse_pos):
-        super().pressed_button(rect,pressed_mouse,mouse_pos)
-        if pressed_mouse[0] and not rect.collidepoint(mouse_pos):self.states["active"],self.states["presses_touch"]=False,True
-        if self.states["active"]:pass
     def charge_elements(self, elements: list):
         if self.elements is None:
             self.elements = elements
