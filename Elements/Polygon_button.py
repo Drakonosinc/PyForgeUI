d25c965e0daf896b01154112b3d2e9825687c2e1
matias <mat20can@gmail.com>
2025-05-20 15:09:28 -0300

    version 1

---
+++ Elements/Polygon_button.py
@@ -0,0 +1,10 @@
+class PolygonButton(ElementBehavior):
+    def __init__(self,config:dict):
+        super().__init__(config)
+        self.screen = config["screen"]
+        self.position = config["position"]
+        self.hover_position = config.get("hover_position",self.position)
+        self.color = config.get("color", (255, 255, 255))
+        self.hover_color = config.get("hover_color", (255, 199, 51))
+        self.detect_mouse=config.get("detect_mouse",True)
+        self.rect = pygame.draw.polygon(self.screen, self.color, self.position).copy()
