88ee89957f5fa839b49bdd67d54a3f784ffc2703
matias <mat20can@gmail.com>
2025-05-20 14:53:24 -0300

    version 1

---
+++ Elements/Element_behavior.py
@@ -0,0 +1,9 @@
+class ElementBehavior:
+    def __init__(self, config: dict):
+        self.sound_hover = config.get("sound_hover")
+        self.sound_touch = config.get("sound_touch")
+        self.detect_mouse=config.get("detect_mouse",True)
+        self.pressed = config.get("pressed",True)
+        self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"click_time": None,"active":False})
+        self.commands = [config.get(f"command{i}") for i in range(1,4)]
+        self.new_events(time=config.get("time",500))
--- Elements/Elements_interface.py
+++ Elements/Elements_interface.py
@@ -1,14 +1,6 @@
 import pygame
 from pygame.locals import *
-class ElementBehavior:
-    def __init__(self, config: dict):
-        self.sound_hover = config.get("sound_hover")
-        self.sound_touch = config.get("sound_touch")
-        self.detect_mouse=config.get("detect_mouse",True)
-        self.pressed = config.get("pressed",True)
-        self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"click_time": None,"active":False})
-        self.commands = [config.get(f"command{i}") for i in range(1,4)]
-        self.new_events(time=config.get("time",500))
+    
     def events(self, event):pass
     def new_events(self,time):
         self.EVENT_NEW = pygame.USEREVENT + self.define_event()
