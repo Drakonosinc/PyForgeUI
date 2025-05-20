041425f2d3faaccf8f8cd1a326b773f7a5be68fa
matias <mat20can@gmail.com>
2025-05-20 15:23:17 -0300

    version 1

--- Elements/Combo_box.py
+++ Elements/Combo_box.py
@@ -24,4 +24,6 @@ class ComboBox(ElementBehavior):
         super().pressed_button(rect,pressed_mouse,mouse_pos)
         if pressed_mouse[0] and not rect.collidepoint(mouse_pos):self.states["active"],self.states["presses_touch"]=False,True
         if self.states["active"]:pass
-    
+    def charge_elements(self, elements: list):
+        if self.elements is None:
+            self.elements = elements
--- Elements/Elements_interface.py
+++ Elements/Elements_interface.py
@@ -1,5 +1,3 @@
 import pygame
 from pygame.locals import *
-    def charge_elements(self, elements: list):
-        if self.elements is None:
-            self.elements = elements
+    
