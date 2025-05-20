56663db15a58b6a823917d9316e6e8b89756996a
matias <mat20can@gmail.com>
2025-05-20 15:09:20 -0300

    version 1

--- Elements/Elements_interface.py
+++ Elements/Elements_interface.py
@@ -2,16 +2,7 @@ import pygame
 from pygame.locals import *
 
 
-class PolygonButton(ElementBehavior):
-    def __init__(self,config:dict):
-        super().__init__(config)
-        self.screen = config["screen"]
-        self.position = config["position"]
-        self.hover_position = config.get("hover_position",self.position)
-        self.color = config.get("color", (255, 255, 255))
-        self.hover_color = config.get("hover_color", (255, 199, 51))
-        self.detect_mouse=config.get("detect_mouse",True)
-        self.rect = pygame.draw.polygon(self.screen, self.color, self.position).copy()
+
     def draw(self):
         pygame.draw.polygon(self.screen, self.color, self.position)
         if self.detect_mouse:self.mouse_collision(self.rect,pygame.mouse.get_pos())
