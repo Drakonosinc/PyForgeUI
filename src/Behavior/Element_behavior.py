4b79970ae74ee15ed7ab7427dc8219d87083400c
matias <mat20can@gmail.com>
2025-05-22 16:30:29 -0300

    version 1

--- src/Behavior/Element_behavior.py
+++ src/Behavior/Element_behavior.py
@@ -2,8 +2,6 @@ import pygame
 from pygame.locals import *
 class ElementBehavior:
     def __init__(self, config: dict):
-        self.detect_mouse=config.get("detect_mouse",True)
-        self.pressed = config.get("pressed",True)
         self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"click_time": None,"active":False})
         self.commands = [config.get(f"command{i}") for i in range(1,4)]
         self.new_events(time=config.get("time",500))
