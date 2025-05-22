1007c48ca1f5c96233ad9f83c8e8974fbec61841
matias <mat20can@gmail.com>
2025-05-22 16:30:20 -0300

    version 1

--- src/Behavior/Element_behavior.py
+++ src/Behavior/Element_behavior.py
@@ -2,8 +2,6 @@ import pygame
 from pygame.locals import *
 class ElementBehavior:
     def __init__(self, config: dict):
-        self.sound_hover = config.get("sound_hover")
-        self.sound_touch = config.get("sound_touch")
         self.detect_mouse=config.get("detect_mouse",True)
         self.pressed = config.get("pressed",True)
         self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"click_time": None,"active":False})
