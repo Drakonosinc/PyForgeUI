6c7903743ab9e78d9a041817a7d0cb9e6b46d682
matias <mat20can@gmail.com>
2025-05-22 16:40:02 -0300

    version 1

--- src/Behavior/Element_behavior.py
+++ src/Behavior/Element_behavior.py
@@ -4,6 +4,7 @@ from ..Behavior import *
 class ElementBehavior(MouseCollision,PressedButton):
     def __init__(self, config: dict):
         MouseCollision.__init__(self, config)
+        PressedButton.__init__(self, config)
         self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"click_time": None,"active":False})
         self.commands = [config.get(f"command{i}") for i in range(1,4)]
         self.new_events(time=config.get("time",500))
