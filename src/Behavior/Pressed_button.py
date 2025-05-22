5b74f4dc6695ca51c4d7892f6a2a6f8efacb4ce8
matias <mat20can@gmail.com>
2025-05-22 16:42:08 -0300

    version 1

--- src/Behavior/Pressed_button.py
+++ src/Behavior/Pressed_button.py
@@ -2,3 +2,4 @@ class PressedButton:
     def __init__(self,config: dict):
         self.sound_touch = config.get("sound_touch")
         self.pressed = config.get("pressed",True)
+        self.states=config.get("states",{"presses_touch":True,"click_time": None,"active":False})
