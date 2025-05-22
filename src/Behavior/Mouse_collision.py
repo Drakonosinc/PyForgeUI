3e05855f46a57e981dd6d9522448b2b82fe90b2c
matias <mat20can@gmail.com>
2025-05-22 16:26:15 -0300

    version 1

--- src/Behavior/Mouse_collision.py
+++ src/Behavior/Mouse_collision.py
@@ -1,4 +1,4 @@
 class MouseCollision:
     def __init__(self,config: dict):
         self.sound_hover = config.get("sound_hover")
-        
+        self.detect_mouse=config.get("detect_mouse",True)
