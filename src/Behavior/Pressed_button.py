0e2d66078b231d85f24a7009e42e6fb972dbb891
matias <mat20can@gmail.com>
2025-05-23 15:58:28 -0300

    version 1

--- src/Behavior/Pressed_button.py
+++ src/Behavior/Pressed_button.py
@@ -18,6 +18,8 @@ class PressedButton:
                 self.states["click_time"] = None
                 self.states["presses_touch"] = True
                 self.execute_commands()
+        if pressed_mouse[0] and not rect.collidepoint(mouse_pos):self.states["active"],self.states["presses_touch"]=False,True
+        if self.states["active"]:pass
     def execute_commands(self):
         try:
             for command in self.commands:
