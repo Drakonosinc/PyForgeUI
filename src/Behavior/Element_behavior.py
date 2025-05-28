a7fe919eb7d8dd8f5923b1bb30fc29a1c9ef5cda
matias <mat20can@gmail.com>
2025-05-28 15:35:37 -0300

    version 10.0

--- src/Behavior/Element_behavior.py
+++ src/Behavior/Element_behavior.py
@@ -8,6 +8,8 @@ class ElementBehavior(MouseCollision,PressedButton):
         self.commands = [config.get(f"command{i}") for i in range(1,4)]
         self.new_events(time=config.get("time",500))
     def events(self, event):pass
+    def reactivate_pressed(self,event):
+        if event.type==self.EVENT_NEW:self.states["presses_touch"]=True
     def new_events(self,time):
         self.EVENT_NEW = pygame.USEREVENT + self.define_event()
         pygame.time.set_timer(self.EVENT_NEW,time)
