27e2442920b624a68ee5f02262313f7ffbb28691
matias <mat20can@gmail.com>
2025-05-29 14:58:34 -0300

    version 10.0

--- src/Elements/Combo_box.py
+++ src/Elements/Combo_box.py
@@ -12,7 +12,7 @@ class ComboBox(ElementBehavior):
         self.hover_color=config.get("hover_color",(255, 199, 51))
         self.hover_dropdown=config.get("hover_dropdown",(135,206,235))
         self.elements = None
-        self.rect = pygame.Rect(*self.position,*self.font.size(self.text+self.type_dropdown))
+        self.rect = pygame.Rect(*self.position,*self.font.size(self.text))
         self.rect_dropdown = pygame.Rect(self.position[0]+self.font.size(self.text)[0], self.position[1],*self.font.size(self.type_dropdown))
     def icon_dropdown(self,type_dropdown):
         match type_dropdown:
