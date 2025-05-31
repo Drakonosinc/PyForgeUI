1c54e7ed500cebb572baff7d76dd7fd5037dc940
matias <mat20can@gmail.com>
2025-05-31 16:48:50 -0300

    version 10.0

--- src/Elements/Combo_box.py
+++ src/Elements/Combo_box.py
@@ -30,7 +30,7 @@ class ComboBox(ElementBehavior):
         if self.pressed:self.pressed_button(self.rect["dropdown"],pygame.mouse.get_pressed(),pygame.mouse.get_pos(),self.draw_pressed_effect2)
     def draw_hover_effect1(self):return self.screen.blit(self.font.render(f"{self.text}{self.type_dropdown}", True,self.hover_color), (self.position))
     def draw_hover_effect2(self):return self.screen.blit(self.font.render(f"{self.type_dropdown}",True,self.hover_dropdown), (self.position[0]+self.font.size(self.text)[0], self.position[1]))
-    
+    def draw_pressed_effect1(self):pass
     def charge_elements(self, elements: list):
         if self.elements is None:
             self.elements = elements
