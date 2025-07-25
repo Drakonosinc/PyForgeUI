a9a0e7db6e584f1346d02e22acec393240130de2
matias <mat20can@gmail.com>
2025-07-25 16:57:25 -0300

    version 10.0

--- src/Elements/Text.py
+++ src/Elements/Text.py
@@ -1,9 +1,9 @@
-from ..Behavior.Element_behavior import *
-class Text:
+import pygame
+from ..Behavior.Mouse_collision import MouseCollision
+class Text(MouseCollision):
     def __init__(self,config:dict):
         self.screen = config["screen"]
         self.font = config.get("font", pygame.font.Font(None, 25))
-        self.Behavior = ElementBehavior(config)
         self.text = config["text"]
         self.color = config.get("color", (255, 255, 255))
         self.hover_color = config.get("hover_color", (255, 199, 51))
@@ -11,7 +11,7 @@ class Text:
         self.rect = pygame.Rect(*self.position, *self.font.size(self.text))
     def draw(self):
         self.screen.blit(self.font.render(self.text, True,self.color), self.position)
-        if self.Behavior.detect_mouse:self.Behavior.mouse_collision(self.rect,pygame.mouse.get_pos(),self.draw_hover_effect)
+        if self.detect_mouse:self.mouse_collision(self.rect,pygame.mouse.get_pos(),self.draw_hover_effect)
     def draw_hover_effect(self):return self.screen.blit(self.font.render(self.text,True,self.hover_color),self.position)
     def change_item(self,config:dict):
         self.position = config.get("position",self.position)
