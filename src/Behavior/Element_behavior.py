0a736381f3a2b09553d4f9331f26b58e926d3e10
matias <mat20can@gmail.com>
2025-07-28 16:39:49 -0300

    version 10.0

--- src/Behavior/Element_behavior.py
+++ src/Behavior/Element_behavior.py
@@ -14,4 +14,8 @@ class ElementBehavior(MouseCollision,PressedButton):
         pygame.time.set_timer(self.EVENT_NEW,time)
     def define_event(self):return 1
     def filter_rects_collision(self,rects: dict, mouse_pos, draws: list, option: bool=False):
-        
+        for rect, draw in zip(rects, draws):
+            if rects[rect].collidepoint(mouse_pos):
+                if option is True: self.pressed_button(rects[rect], pygame.mouse.get_pressed(), mouse_pos, draw)
+                else:self.mouse_collision(rects[rect], mouse_pos, draw)
+            if all(not rects[rect].collidepoint(mouse_pos) for rect in rects):self.states["detect_hover"],self.states["presses_touch"] = True,True
