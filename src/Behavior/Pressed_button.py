3a0c558c2a83ba7582a989bf4a76ecb846833d25
matias <mat20can@gmail.com>
2025-09-17 16:39:02 -0300

    version 10.0

--- src/Behavior/Pressed_button.py
+++ src/Behavior/Pressed_button.py
@@ -5,7 +5,25 @@ class PressedButton:
         self.pressed = config.get("pressed",True)
         self.states=config.get("states",{"presses_touch":True,"click_time": None,"active":False})
         self.commands = [config.get(f"command{i}") for i in range(1,config.get("number_commands", 4))]
-    
+    def pressed_button(self,rect,pressed_mouse,mouse_pos,draw=None,repeat:bool = None):
+        repeat = self.repeat if repeat is None else repeat
+        current_time = pygame.time.get_ticks()
+        if pressed_mouse[0] and rect.collidepoint(mouse_pos) and self.states["presses_touch"]:
+            self.states["active"]=True
+            self.states["presses_touch"]=False
+            self.states["click_time"] = current_time
+        def execute():
+            if self.sound_touch:self.sound_touch.play(loops=0)
+            self.states["active"] = False
+            self.states["click_time"] = None
+            self.states["presses_touch"] = True
+            self.execute_commands()
+        if not repeat and not pressed_mouse[0] and self.states["active"]:
+            if rect.collidepoint(mouse_pos):execute()
+        elif self.states["click_time"] is not None and repeat:
+            if current_time - self.states["click_time"] >= 200:execute()
+        if pressed_mouse[0] and not rect.collidepoint(mouse_pos) and self.states["active"]:self.states["active"],self.states["presses_touch"]=False,True
+        if self.states["active"]:self.draw_pressed_effect() if draw is None else draw()
     def draw_pressed_effect(self):return NotImplementedError
     def execute_commands(self):
         try:
