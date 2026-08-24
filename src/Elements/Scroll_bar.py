8576646ed96e62e5e85daa8cf357e7e5b9004673
matias <mat20can@gmail.com>
2026-08-24 15:41:58 -0300

    version 10

--- src/Elements/Scroll_bar.py
+++ src/Elements/Scroll_bar.py
@@ -16,10 +16,18 @@ class ScrollBar(ElementBehavior):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if self.thumb_rect.collidepoint(event.pos):
                 self.dragging = True
-
+                self.drag_offset = (event.pos[1] - self.thumb_rect.y) if self.type_of_orientation == "vertical" else (event.pos[0] - self.thumb_rect.x)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1: self.dragging = False
+        elif event.type == pygame.MOUSEMOTION and self.dragging:
+            if self.type_of_orientation == "vertical":
+                new_pos = event.pos[1] - self.drag_offset
+                new_pos = max(self.rect["rect"].top, min(new_pos, self.rect["rect"].bottom - self.thumb_size))
+                self.thumb_rect.y = new_pos
+            else:
+                new_pos = event.pos[0] - self.drag_offset
+                new_pos = max(self.rect["rect"].left, min(new_pos, self.rect["rect"].right - self.thumb_size))
+                self.thumb_rect.x = new_pos
             self.scroll_elements()
-    def scroll_elements(self):
-        max_scroll = self.content_size
 
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect["rect"])
