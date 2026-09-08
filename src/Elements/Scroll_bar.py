26a5a1ecabb2131a017214853d9278edcc09b53f
matias <mat20can@gmail.com>
2026-09-08 13:50:07 -0300

    version 10

--- src/Elements/Scroll_bar.py
+++ src/Elements/Scroll_bar.py
@@ -50,6 +50,9 @@ class ScrollBar(ElementBehavior):
                 new_pos = x0 - offset
                 el.position = (new_pos, y0)
             delta = new_pos - old_pos
+            def add_delta(item, delta):
+                if isinstance(item, pygame.Rect):
+                    if self.type_of_orientation == "vertical": item.y += delta
 
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect["rect"])
